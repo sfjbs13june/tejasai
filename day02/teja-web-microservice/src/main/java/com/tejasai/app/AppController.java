9566cb05b3f74ce5e9a4853aea87ad0f58bf08f5
sfjbs <tejchennamsetti@gmail.com>
2023-09-04 09:00:28 +0000

    Microservice and restcontroller 3rd commit

--- day02/teja-web-microservice/src/main/java/com/tejasai/app/AppController.java
+++ day02/teja-web-microservice/src/main/java/com/tejasai/app/AppController.java
@@ -1,11 +1,6 @@
 package com.tejasai.app;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppController {
